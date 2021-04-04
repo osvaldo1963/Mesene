5a7a31fbd5aa212ce9a18d3d46ceb4ae80ec360d
osvaldo <osvaldo@mintstudioapp.com>
2021-04-04 05:52:49 -0700

    group chat ui done

--- src/Components/Cell/Multicell.js
+++ src/Components/Cell/Multicell.js
@@ -8,34 +8,24 @@ const DATA = [
     {url: "https://images.unsplash.com/photo-1532074205216-d0e1f4b87368?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2181&q=80"},
     {url: "https://images.unsplash.com/photo-1532074205216-d0e1f4b87368?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2181&q=80"},
 ];
-const RenderImages = ({data}) => {
-    console.log(data)
-    var i;
-    for(i =0; i< DATA.length; i++) {
-        console.log(i)
-        return (
-            <View >
-                <Thumbnail 
-                    square
-                    style={styles.thumbnail}
-                    source={{ uri: DATA[i].url}} />
-                
-                <View style={{position: 'absolute', top: 0, bottom: 0, left: 0, right:0, backgroundColor: 'black', borderRadius: 25}}>
-                    <Text style={{color: 'white'}}>{data.length}</Text>
-                </View>
-            </View>
-        )
-    }
-}
+
 const MultiCell = ({ item, onPress }) => {
     return(
         <ListItem avatar onPress={onPress}>
-            <Left>
-                
-            </Left>
             <Body>
-                <View style={styles.bodyStyle}>
-                    <RenderImages data={DATA} />   
+            <View style={styles.bodyStyle}>
+                    {
+                        DATA.map((item, index) => {
+                            return(
+                                <Thumbnail
+                                    key={index} 
+                                    square
+                                    style={styles.thumbnail}
+                                    source={{ uri: item.url}} />
+                            )
+                        }).slice(0, 3)
+                    }   
+                    <Text note>{`${DATA.length} group`}</Text>
                 </View>
             </Body>
             <Right>
@@ -47,7 +37,7 @@ const MultiCell = ({ item, onPress }) => {
 const styles = StyleSheet.create({
     thumbnail: {
         borderRadius: 25,   
-        marginLeft: -4
+        marginLeft: -25
     }, 
     bodyStyle: {
         flexDirection:'row',
